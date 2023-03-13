3206af190982a93dbdf66182e71fbf03f8213d66
RonaldPombo <ronaldsljr16@gmail.com>
2023-03-13 23:17:19 +0900

    script modificado

--- www/components/mainScript.js
+++ www/components/mainScript.js
@@ -1,3 +1,8 @@
-function olamundo(){
-    alert("Olá mundo!");
+function regrade3(){
+    var valor1 = document.getElementById('valor1')
+    var valor2 = document.getElementById('valor1')
+    var valor3 = document.getElementById('valor1')
+    var valor4 = 
+    var x = value4 / valor1
+    document.getElementById('regra').innerHTML = 'Novo valor que quero que apareça';
 }
